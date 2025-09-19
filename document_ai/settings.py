69d7df9bd9ec47ed408a9c809be55868adeeeb84
Marco Epicoco <marcoepicoco2@gmail.com>
2025-09-19 12:27:19 +0200

    3 commit

--- document_ai/settings.py
+++ document_ai/settings.py
@@ -13,7 +13,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'documents',
+    'document_ai.documents', 
 ]
 
 MIDDLEWARE = [
--- document_ai/urls.py
+++ document_ai/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('documents.urls')),
+    path('', include('document_ai.documents.urls')),  
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
